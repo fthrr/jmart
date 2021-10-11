f4d8baa6e83fa292b2710d06ca2ab5be3856bd91
fthrr <fathurrahman.irwansa@ui.ac.id>
2021-10-11 21:15:40 +0700

    PT - Modul 5 - Fathurrahman Irwansa

--- fathurJmartMR/Shipment.java
+++ fathurJmartMR/Shipment.java
@@ -13,69 +13,71 @@ import java.text.SimpleDateFormat;
 public class Shipment
 {
 	public static final SimpleDateFormat ESTIMATION_FORMAT = new SimpleDateFormat("E MMMM dd yyyy");
-    public static final Duration INSTANT = new Duration((byte) (1 << 0));
-    public static final Duration SAME_DAY = new Duration((byte) (1 << 1));
-    public static final Duration NEXT_DAY = new Duration((byte) (1 << 2));
-    public static final Duration REGULER = new Duration((byte) (1 << 3));
-    public static final Duration KARGO = new Duration((byte) (1 << 4));
+    public static final Plan INSTANT = new Plan((byte) (1 << 0));
+    public static final Plan SAME_DAY = new Plan((byte) (1 << 1));
+    public static final Plan NEXT_DAY = new Plan((byte) (1 << 2));
+    public static final Plan REGULER = new Plan((byte) (1 << 3));
+    public static final Plan KARGO = new Plan((byte) (1 << 4));
     public String address;
     public int cost;
     public byte plan;
     public String receipt;
     
-    public static class Duration{
+    public static class Plan{
         private final byte bit;
         
-        public static final SimpleDateFormat ESTIMATION_FORMAT = new SimpleDateFormat("E MMMM dd yyyy");
-        public static final Duration INSTANT = new Duration((byte) (1 << 0));
-        public static final Duration SAME_DAY = new Duration((byte) (1 << 1));
-        public static final Duration NEXT_DAY = new Duration((byte) (1 << 2));
-        public static final Duration REGULER = new Duration((byte) (1 << 3));
-        public static final Duration KARGO = new Duration((byte) (1 << 4));
-        
-        private Duration (byte bit){
+        private Plan (byte bit){
             this.bit = bit;
-        }
-        
-        public String getEstimatedArrival(Date reference){
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(reference);
-            if (bit == Duration.INSTANT.bit || bit == Duration.SAME_DAY.bit){
-                cal.add(Calendar.DATE, 0);
-            }
-            else if (bit == Duration.NEXT_DAY.bit){
-                cal.add(Calendar.DATE, 1);
-            }
-            else if (bit == Duration.REGULER.bit){
-                cal.add(Calendar.DATE, 2);
-            }
-            else if (bit == Duration.KARGO.bit){
-                cal.add(Calendar.DATE, 5);
-            }
-            return ESTIMATION_FORMAT.format(cal.getTime());
         }
     }
     
-    public class MultiDuration{
+    public Shipment(String address, int cost, byte plan, String receipt){
+        this.address = address;
+        this.cost = cost;
+        this.plan = plan;
+        this.receipt = receipt;
+    }
+    
+    public String getEstimatedArrival(Date reference){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(reference);
+        if (plan == INSTANT.bit || plan == SAME_DAY.bit){
+            cal.add(Calendar.DATE, 0);
+        }
+        else if (plan == NEXT_DAY.bit){
+            cal.add(Calendar.DATE, 1);
+        }
+        else if (plan == REGULER.bit){
+            cal.add(Calendar.DATE, 2);
+        }
+        else if (plan == KARGO.bit){
+            cal.add(Calendar.DATE, 5);
+        }
+        return ESTIMATION_FORMAT.format(cal.getTime());
+    }
+    
+    public boolean isDuration (Plan reference){ 
+        return (this.plan & reference.bit) != 0;
+    }
+    
+    public static boolean isDuration(byte object, Plan reference) {
+    	if((object & reference.bit) == 0) {
+    		return false;
+    	}
+    	return true;
+    }
+    
+    /*public class MultiDuration{
         public byte bit;
         
-        public MultiDuration(Duration... args){
+        public MultiDuration(Plan... args){
             byte flags = 0;
             for (int i = 0; i < args.length; ++i)
                 flags |= args[i].bit;
             bit = flags;
         }
-        public boolean isDuration(Duration reference) { 
-            return (this.bit & reference.bit) != 0;
-        }
-    }
-    
-    public Shipment(String address, int shipmentCost, Duration duration, String receipt){
-        /*this.address = address;
-        this.shipmentCost = shipmentCost;
-        this.duration = duration;
-        this.receipt = receipt;*/
-    }
+        
+    }*/
     
     /*@Override
     public boolean read(String content){
